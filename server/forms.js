2b756cf768c8cba03f1a1e9ffcbe6754dde33f87
Chris Friel <cfriel@gmail.com>
2013-08-01 02:02:04 -0400

    registering template

--- server/forms.js
+++ server/forms.js
@@ -1,20 +1,16 @@
+var name = "forms";
+var version = "1.0";
+
 Meteor.startup(function () {
 
-    Databases = new Meteor.Collection("databases");
-
-    Databases.remove({});
-
-    Meteor.publish("databases", function(){
-	return Databases.find();
-    });
-
-    Data = new Meteor.Collection("data");
-
-    Data.remove();
-
-    Meteor.publish("data", function(){
-	return Data.find();
-    });
+    console.log("loading forms package");
+    
+    if(!SiteTemplates.findOne( { name: name, version: version }))
+    {
+	console.log("registering " + name + " site template");
+	SiteTemplates.insert( { name : name, version : version } );
+	Greenlight.register_template(name, version);
+    }
     
 });
 
