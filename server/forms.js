4210973875e143094fc628098dac3fe4a681b3c2
Chris Friel <cfriel@gmail.com>
2013-08-02 16:06:54 -0400

    refactoring package api

--- server/forms.js
+++ server/forms.js
@@ -5,12 +5,7 @@ Meteor.startup(function () {
 
     console.log("loading forms package");
     
-    if(!SiteTemplates.findOne( { name: name, version: version }))
-    {
-	console.log("registering " + name + " site template");
-	SiteTemplates.insert( { name : name, version : version } );
-	Greenlight.register_template(name, version);
-    }
+    Greenlight.register_template(name, version);
     
 });
 
