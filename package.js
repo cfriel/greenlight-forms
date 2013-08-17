b30260307f7a590af0d4fa72defedd404edbeb02
Chris Friel <cfriel@gmail.com>
2013-08-17 18:55:47 -0400

    updating meteor version

--- package.js
+++ package.js
@@ -7,6 +7,10 @@ Npm.depends({mongodb: "1.3.4"});
 Package.on_use(function (api, where) {
     api.use('router', ['client', 'server']);
     api.use(['templating'], 'client');
+    api.use('deps', ['client', 'server']);
+    api.use('session', ['client', 'server']);
+    api.use('greenlight', ['client','server']);
+
     api.add_files(['client/forms_data_browser_list_item.html', 'client/forms_data_browser_list_item.js'], 'client');
     api.add_files(['client/forms_data_browser.html', 'client/forms_data_browser.css','client/forms_data_browser.js'], 'client');
     api.add_files(['client/forms_page.html','client/forms_page.js', 'client/forms_page.css'], 'client' );
